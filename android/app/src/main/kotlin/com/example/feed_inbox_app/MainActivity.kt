46b54f6362463f94ba501f4fd2abc202d8e300a5
luosen <heyrosen@163.com>
2023-02-12 21:35:05 +0800

    使用getx创建工作目录

---
+++ android/app/src/main/kotlin/com/example/feed_inbox_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.feed_inbox_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/routers/pages.dart
@@ -0,0 +1,14 @@
+import 'package:feed_inbox_app/pages/system/login/index.dart';
+import 'package:get/get.dart';
+
+// 路由 Pages
+class RoutePages {
+  // 列表
+  static List<GetPage> list = [
+    GetPage(
+      name: "/",
+      page: () => const LoginPage(),
+      binding: LoginBinding(),
+    ),
+  ];
+}
