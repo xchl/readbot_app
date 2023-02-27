70a44690e2b58b94b3f03b769aeaca84c68f701d
luosen <heyrosen@163.com>
2023-02-27 14:24:22 +0800

    搭建基础页面和路由

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:feed_inbox_app/common/index.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,21 +17,13 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+
       // 路由
-      initialRoute: "/",
+      initialRoute: RouteNames.systemSplash,
       getPages: RoutePages.list,
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      navigatorObservers: [RoutePages.observer],
     );
   }
 }
