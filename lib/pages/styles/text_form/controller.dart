54702d76fbf46bf2ef461e01951ff9f13a067a7c
luosen <heyrosen@163.com>
2023-03-02 10:21:14 +0800

    添加form widget

--- lib/pages/styles/text_form/controller.dart
+++ lib/pages/styles/text_form/controller.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class TextFormController extends GetxController {
   TextFormController();
+
+  GlobalKey formKey = GlobalKey<FormState>();
+
+  TextEditingController unameController =
+      TextEditingController(text: "ducafecat");
+  TextEditingController pwdController = TextEditingController(text: "123456");
 
   _initData() {
     update(["text_form"]);
@@ -18,6 +25,13 @@ class TextFormController extends GetxController {
   void onReady() {
     super.onReady();
     _initData();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    unameController.dispose();
+    pwdController.dispose();
   }
 
   // @override
