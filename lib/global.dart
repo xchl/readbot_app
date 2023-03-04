73ad3b4643452ac41511ec08de970061e44a93a7
luosen <heyrosen@163.com>
2023-03-04 15:16:56 +0800

    菜单栏反色 & 保持竖直

--- lib/global.dart
+++ lib/global.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
@@ -12,6 +13,9 @@ class Global {
     // 让启动图保持在屏幕
     FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
+    // 系统样式
+    setSystemUi();
+
     // 工具类
     await Storage().init();
     Loading();
@@ -23,5 +27,43 @@ class Global {
 
     // 默认语言
     ConfigService.to.onLocaleUpdate(Translation.fallbackLocale);
+  }
+
+  // 系统样式
+  static void setSystemUi() {
+    if (GetPlatform.isMobile) {
+      // 屏幕方向 竖直上
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+      // 透明状态栏
+      // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      //   statusBarColor: Colors.transparent, // transparent status bar
+      // ));
+    }
+
+    if (GetPlatform.isAndroid) {
+      // 去除顶部系统下拉和底部系统按键
+      // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+      // 去掉底部系统按键
+      // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      //     overlays: [SystemUiOverlay.bottom]);
+
+      // 自定义样式
+      SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
+        // 顶部状态栏颜色
+        statusBarColor: Colors.transparent,
+        // 该属性仅用于 iOS 设备顶部状态栏亮度
+        // statusBarBrightness: Brightness.light,
+        // 顶部状态栏图标的亮度
+        // statusBarIconBrightness: Brightness.light,
+
+        // 底部状态栏与主内容分割线颜色
+        systemNavigationBarDividerColor: Colors.transparent,
+        // 底部状态栏颜色
+        systemNavigationBarColor: Colors.white,
+        // 底部状态栏图标样式
+        systemNavigationBarIconBrightness: Brightness.dark,
+      );
+      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    }
   }
 }
